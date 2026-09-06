e9f3c552198a744c8335b97d1d4bf6f3751a5ffa
eumaeus <cwblackwell@gmail.com>
2026-09-06 06:44:21 -0400

    Update demo_ellipsis.cex for tokenized verse references and add sync script for deployment

---
+++ sync.sh
@@ -0,0 +1,1 @@
+rsync -avz --progres --exclude='ai_queries' --exclude='.git' --exclude='.gitignore' --exclude='.DS_Store' /Users/cblackwell/cite/grok/Syntactile/src/  cblackwell@folio3.furman.edu:~/syntax/ 
